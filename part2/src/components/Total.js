7791c36f3919a125adb85fb8093732f04b7278fa
RodrigoH95 <HRodrigo95@hotmail.com>
2022-12-18 01:51:26 -0300

    part2 course information completed

--- part2/src/components/Total.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Total = ({ parts }) => (
-    <>
-      <p>Total of {parts.reduce((acc, part) => acc + part.exercises, 0)} exercises.</p>
-    </>
-  )
-
-export default Total
